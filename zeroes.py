490bbd271d60f7b3fc3e0e39279bbb63b652d742
vignesh <vickichinna1234@gmail.com>
2024-10-18 23:30:34 +0530

    zeros value get updated
    edited

--- zeroes.py
+++ zeroes.py
@@ -1,9 +1,25 @@
-L=list(map(int,input().split()))
-L1=[]
-L2=[]
-for i in L:
-  if i!=0:
-    L1+=[i]
-  else:
-    L2+=[i]
-print(L1+L2)
+def move_zeros_to_end(arr):
+    # Pointer for the position of the next non-zero element
+    non_zero_index = 0
+
+    # Traverse the array
+    for num in arr:
+        # If the current number is not zero, place it at the non-zero index
+        if num != 0:
+            arr[non_zero_index] = num
+            non_zero_index += 1
+
+    # After all non-zero elements have been moved to the front,
+    # fill the remaining positions with zeros
+    for i in range(non_zero_index, len(arr)):
+        arr[i] = 0
+
+    return arr
+
+# Example usage
+if __name__ == "__main__":
+    user_input = input("Enter numbers separated by spaces: ")
+    arr = [int(num) for num in user_input.split()]
+
+    result = move_zeros_to_end(arr)
+    print("Array after moving zeros to the end:", result)
